1d6c99c7a77ea0fd60eb40535a7689cd68fb029d
Alex Mendes <mendes.develop@gmail.com>
2019-08-20 20:03:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-   if !(array.include?(cheese_types.map{|n| "#{n}" }))
+   if !(cheese_types.each{|n| array.include?(n) })
      return nil
    else 
      puts "cheddar"
