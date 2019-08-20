afe53e81ba8c3707c9bb036d3d27d842a59fec47
Alex Mendes <mendes.develop@gmail.com>
2019-08-20 19:38:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index|
+  puts "#{index} #{item}"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|n| n.capitalize}
+  new_array = array.collect{|n| n + "!" }
+  return new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.map{|n| if (n.length) > 4
+    return true
+    end
+  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+   
+  array.include?{|n| n == cheese_types.each
+    return n
+  }
 end
